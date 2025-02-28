20f7059e2e00a432335dd8151a0bbb83ae3d945d
mcsaunders02 <mcsaunders02@wm.edu>
2025-02-28 18:17:52 -0500

    Added comment for screen field

--- include/PlayerView.hpp
+++ include/PlayerView.hpp
@@ -12,6 +12,7 @@ class PlayerView {
     SDL_Renderer* renderer;
 
     // Pointer to the current screen, it needs a pointer because Screen is an abstract class
+    // This needs to be set in the constructor
     std::unique_ptr<Screen> screen;
 
     public:
