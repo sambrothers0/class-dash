362ecbb356fb1e181df2ab79ed492b2368910555
Kornel <ketyler@wm.edu>
2025-03-10 22:19:10 -0400

    Add sprite class functionality and fix bugs

--- include/Sprite.hpp
+++ include/Sprite.hpp
@@ -2,7 +2,7 @@
 #define _SPRITE_H
 
 #include "SDL.h"
-#include "Vector2.hpp"
+#include "physics/Vector2.hpp"
 
 class Sprite {
 
@@ -10,31 +10,30 @@ class Sprite {
         SDL_Texture* sprite;
         SDL_Rect textureRect;
         SDL_Rect targetRect;
-        Vector2 position;
-        double scale;
+        unsigned int depth;
+        
 
     public:
         Sprite(SDL_Texture* _sprite);
 
         // Position methods set position of the sprite
+        // These methods can be used for setting inital position and for moving positions
         void setPosition(double x, double y);
 
         void setPosition(Vector2 newPosition);
 
-        void movePositon();
 
-        // Sets the dimentions of the sprite
-        void setDimensions(double x, double y);
+        // Sets the dimensions of the sprite
+        void setDimensions(double width, double height);
+
+        // Primarily for setting the textures using a sprite map
+        void setTexture(double x, double y, double width, double height);
 
 
-        // Scale methods scale the width and height of the sprite
-        void setScale(double newScale);
+        // Scale method scales the width and height of the sprite
+        void setScale(double _scale);
 
-        void scale(double _scale);
-
-        void setAngle();
-
-        void rotate();
+        void setDepth(unsigned int newDepth);
 
 
 };
--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -1,5 +1,45 @@
 #include "Sprite.hpp"
 
-Sprite::Sprite(SDL_Texture* _sprite) {
+Sprite::Sprite(SDL_Texture* _sprite) : sprite(_sprite) {
+    targetRect.x = 0;
+    targetRect.y = 0;
+    textureRect.x = 0;
+    textureRect.y = 0;
+    SDL_QueryTexture(sprite, NULL, NULL, &textureRect.w, &textureRect.h);
+}
 
+void Sprite::setPosition(double x, double y) {
+    targetRect.x = x;
+    targetRect.y = y;
+}
+
+// Can be used for setting inital position or for sprite movement
+void Sprite::setPosition(Vector2 newPosition) {
+    targetRect.x = newPosition.getX();
+    targetRect.y = newPosition.getY();
+}
+
+// Sets the dimensions of the sprite
+void Sprite::setDimensions(double width, double height) {
+    targetRect.w = width;
+    targetRect.h = height;
+}
+
+void Sprite::setTexture(double x, double y, double width, double height) {
+    textureRect.x = x;
+    textureRect.y = y;
+    textureRect.w = width;
+    textureRect.h = height;
+}
+
+// Scale methods scale the width and height of the sprite
+
+void Sprite::setScale(double _scale) {
+    targetRect.w *= textureRect.w * _scale;
+    targetRect.h *= textureRect.h * _scale;
+}
+
+// depth tells when a sprite should be drawn in comparison to others
+void Sprite::setDepth(unsigned int newDepth) {
+    depth = newDepth;
 }
