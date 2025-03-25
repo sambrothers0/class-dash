6e0371128bd1997ac6ae4b2153ad4f26c75592a5
Cass124 <102390731+Cass124@users.noreply.github.com>
2025-03-25 00:44:43 -0400

    Removed temporary Tilesets

--- src/ui/screens/Levels/Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="10" columns="10">
- <image source="../../../../assets/visual/tilesheet.png" width="320" height="32"/>
-</tileset>
