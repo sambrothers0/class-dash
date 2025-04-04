0bea058be4e1fb3102ac8a5da01b7928530a92f5
Kornel <ketyler@wm.edu>
2025-04-04 13:52:11 -0400

    Fix timer display

--- include/TimeKeeper.hpp
+++ include/TimeKeeper.hpp
@@ -10,18 +10,13 @@ class TimeKeeper {
     private:
         int minutes, seconds, startTime;
         Uint64 timeElapsed;
-        bool reset = false;
 
     public:
         TimeKeeper(); // initialize the time 
-        //void startTimer(); // sets startTime to current time when time resets
-        void resetTimer(); // set reset timer by setting the startime to the current time
+        void resetTime(); // set reset timer by setting the startime to the current time
         void updateTime(); // updates the time in the gameloop
 
         std::string getTime();
-        
-        int getMinutes() {return minutes;}
-        int getSeconds() {return seconds;}
     
 };
 
