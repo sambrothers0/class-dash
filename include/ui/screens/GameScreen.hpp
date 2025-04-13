4c3090527902b9f2e819f42e30544cfb7f3fbd45
Kornel <ketyler@wm.edu>
2025-04-13 18:37:23 -0400

    Add texture onto projectile

--- include/ui/screens/GameScreen.hpp
+++ include/ui/screens/GameScreen.hpp
@@ -17,6 +17,7 @@ class GameScreen : public Screen {
 
     // Spritesheet for the player
     Spritesheet playerSprite;
+    Spritesheet objectSprite;
     // std::unordered_map<uint32_t, SDL_Texture*> tilesetTextures;
 
     // Offset for drawing
@@ -46,6 +47,12 @@ class GameScreen : public Screen {
             Vector2(PLAYER_WIDTH, PLAYER_HEIGHT),
             5,
             2
+        ), objectSprite(
+            _renderer,
+            "../assets/visual/objectSpritesheet.png",
+            Vector2(32, 32),
+            1,
+            4
         ) {}
 
     virtual void draw();
--- src/ui/screens/GameScreen.cpp
+++ src/ui/screens/GameScreen.cpp
@@ -77,6 +77,8 @@ void GameScreen::draw() {
 
     playerSprite.draw(PlayerTexture::WALK1 + player->getCurrentAnimationOffset(), playerPosition - Vector2(scrollOffset, 0), player->getLastDirection() == MoveDirection::LEFT);
 
+    bool direction = false;
+
     // Draw the player hitbox
     if (showHitboxes)
         drawCollisionHitbox(playerPosition, player->getHitbox());
@@ -84,7 +86,14 @@ void GameScreen::draw() {
     // Display the projectiles that have been shot
     for (auto proj : player->getProjectiles()) {
         Vector2 projectilePosition = proj.getPosition();
-        boxRGBA(renderer, projectilePosition.getX() - 10 - scrollOffset, projectilePosition.getY() - 10, projectilePosition.getX() + 10 - scrollOffset, projectilePosition.getY() + 10, 0, 255, 255, 255);
+        //boxRGBA(renderer, projectilePosition.getX() - 10 - scrollOffset, projectilePosition.getY() - 10, projectilePosition.getX() + 10 - scrollOffset, projectilePosition.getY() + 10, 0, 255, 255, 255);
+        if (player->getCurrentDirection() == MoveDirection::RIGHT) {
+            direction = false;
+        }
+        else if (player->getCurrentDirection() == MoveDirection::LEFT) {
+            direction = true;
+        }
+        objectSprite.draw(3, projectilePosition - Vector2(scrollOffset, 0), direction);
     }
     
     // Display the Time on the screen
