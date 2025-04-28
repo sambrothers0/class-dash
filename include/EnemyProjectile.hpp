333ac88981c05188b2652d8ceb17055f7ddebbb5
Kornel <ketyler@wm.edu>
2025-04-27 22:24:52 -0400

    Fix a few problems

--- include/EnemyProjectile.hpp
+++ include/EnemyProjectile.hpp
@@ -3,15 +3,20 @@
 
 #include "GameLogic.hpp"
 #include "Projectile.hpp"
+#include "characters/Player.hpp"
 
 
 class EnemyProjectile : public Projectile {
 
     private:
-        Vector2 destination = gameLogic->getPlayer()->getPosition();
+        Vector2 destination;
+
+        double enemyProjTraveledDist = 0;
     public:
 
-    EnemyProjectile(std::shared_ptr<GameLogic> _gameLogic, Vector2 enemyPosition, MoveDirection enemyDirection) : Projectile(_gameLogic, enemyPosition, enemyDirection) {}
+    EnemyProjectile(std::shared_ptr<GameLogic> _gameLogic, Vector2 enemyPosition, MoveDirection enemyDirection) : Projectile(_gameLogic, enemyPosition, enemyDirection) {
+        destination = gameLogic->getPlayer()->getPosition();
+    }
 
     // move projectile towards player
     virtual void move(double ms);
--- src/EnemyProjectile.cpp
+++ src/EnemyProjectile.cpp
@@ -1,5 +1,5 @@
 #include "EnemyProjectile.hpp"
-
+#include "gameDimensions.hpp"
 //EnemyProjectile::EnemyProjectile() {
 
 //}
